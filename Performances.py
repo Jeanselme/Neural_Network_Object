852aab328812470548b8e7225c837a13b1a0b4ae
Vincent Jeanselme <vincent.jeanselme@gmail.com>
2017-01-09 16:36:59 +0100

    Plot training and testing times

--- Performances.py
+++ Performances.py
@@ -5,29 +5,43 @@ import matplotlib.pyplot as plt
 NUM_THREADS = 8
 MAXITER = 10
 
-times = []
+trainingTimes = []
+testingTimes = []
 
-batchSize = [100,500,1000,5000]
-style = ['ro', 'bo', 'go', 'yo']
+batchSize = [100, 500, 1000, 5000, 10000]
+style = ['ro-', 'bo-', 'go-', 'yo-', 'mo-']
 
 for batch in batchSize:
-	timeBatch = []
+	timeTrainBatch = []
+	timeTestBatch = []
 	for i in range(1,NUM_THREADS+1):
-		print(" ====== Executing with : " + str(i) + " threads ====== ")
+		print(" ====== Executing with : " + str(i) + " threads with a batch : " +str(batch)+ " ====== ")
 		result = subprocess.check_output("make clean run VERBOSE=0 OMP_NUM_THREADS=" + str(i) + " BATCH=" + str(batch) + " MAXITER=" + str(MAXITER), shell=True).decode('utf-8')
-		time = float(re.search(r"Training in (?P<time>\d+\.\d+) s",result).group('time'))/MAXITER
-		timeBatch.append(time)
-		print(time)
+		train = float(re.search(r"Training in (?P<time>\d+\.\d+) s",result).group('time'))/(MAXITER*60) # Divison because 60 000 images and results in ms
+		test = float(re.search(r"Testing in (?P<time>\d+\.\d+) s",result).group('time'))/10 # Division because 10 000 images and results in ms
+		timeTrainBatch.append(train)
+		timeTestBatch.append(test)
+		print(train, test)
 
-	times.append(timeBatch)
-plt.figure(1)
+	trainingTimes.append(timeTrainBatch)
+	testingTimes.append(timeTestBatch)
 
-# Training error
+plt.figure("Training")
 for i in range(len(batchSize)):
-	plt.plot(range(1,NUM_THREADS+1), times[i], style[i])
+	plt.plot(range(1,NUM_THREADS+1), trainingTimes[i], style[i], label=str(batchSize[i]))
 plt.xlim(0, NUM_THREADS + 1)
 plt.xlabel('Number of threads')
 plt.ylabel('Time per iteration')
-plt.title('Average training time')
+plt.title('Average training time (s)')
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+
+plt.figure("Testing")
+for i in range(len(batchSize)):
+	plt.plot(range(1,NUM_THREADS+1), testingTimes[i], style[i], label=str(batchSize[i]))
+plt.xlim(0, NUM_THREADS + 1)
+plt.xlabel('Number of threads')
+plt.ylabel('Time per image (ms)')
+plt.title('Average testing time')
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
 plt.show()
